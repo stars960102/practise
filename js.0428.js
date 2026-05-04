43dc57ceb7e238028ae65bfdc8d42246171be35a
zjial <zjialun35@gmail.com>
2026-05-04 15:47:37 +0800

    完整大樂透

--- js.0428.js
+++ js.0428.js
@@ -1,5 +1,5 @@
 var balls=[];
-for (var i = 1; i < 49; i++) {
+for (var i = 1; i <= 49; i++) {
     balls.push(i);
 }
 for(var i  = balls.length-1;i>=0; i--){
@@ -11,7 +11,7 @@ balls[j] = temp;
 var lucky = balls.slice(0,6);
 lucky.sort(function(a, b){
     return a-b;
-})
+});
 var formattedNumbers = [];
 for (var k = 0; k < lucky.length; k++) {
     if (lucky[k] < 10) {
@@ -20,4 +20,4 @@ for (var k = 0; k < lucky.length; k++) {
         formattedNumbers.push(lucky[k].toString());
     }
 }
-console.log("您的電腦選號為：" + formattedNumbers.join(', '))
+console.log("您的電腦選號為：" + formattedNumbers.join(', '));
